8234b95085a31f6c7f1046bc735581882a389799
Darren Kulback <dkulback@gmail.com>
2023-01-17 14:32:00 -0700

    Add scrape/root paths

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  resources :shows
+  resources :shows do
+    match '/scrape', to: 'shows#scrape', via: :post, on: :collection
+  end
+
+  root to: 'shows#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
