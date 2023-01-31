989260678da72eb0171eadc6ede3eace3485b856
Darren Kulback <dkulback@gmail.com>
2023-01-30 17:10:50 -0700

    Add search test

--- spec/models/show_spec.rb
+++ spec/models/show_spec.rb
@@ -12,4 +12,15 @@ RSpec.describe Show, type: :model do
       expect(actual).to eq(expected)
     end
   end
+  describe '::search' do
+    it 'lets the shows by the date header and venue search param' do
+      show0 = Show.create!(date: Date.yesterday, venue: 'Gothic')
+      show1 = Show.create!(date: Date.today, venue: 'Bluebird')
+      show2 = Show.create!(date: Date.tomorrow, venue: 'hi dive')
+      show3 = Show.create!(date: '31 Jan'.to_date.freeze, venue: 'Gothic')
+      actual = Show.search('gothic')
+      expected = [show3]
+      expect(actual).to eq(expected)
+    end
+  end
 end
