38cfbde9a449d75d4acbddbff9a6d410173bc6fd
Darren Kulback <dkulback@gmail.com>
2023-01-30 17:10:29 -0700

    Add search form

--- spec/views/shows/index.html.erb_spec.rb
+++ spec/views/shows/index.html.erb_spec.rb
@@ -33,4 +33,22 @@ RSpec.describe 'shows/index', type: :feature do
       expect(page).to have_content('Successfully scraped urls')
     end
   end
+  describe 'search form' do
+    it 'lets users search by venue name' do
+      Show.create!(date: Date.yesterday, venue: 'Gothic')
+      Show.create!(date: Date.today, venue: 'Bluebird')
+      Show.create!(date: Date.tomorrow, venue: 'hi dive')
+      Show.create!(date: '31 Jan'.to_date.freeze, venue: 'Gothic')
+
+      visit root_path
+      within(:xpath, ".//div[@class='flex items-center border-b border-teal-500 py-2']") do
+        fill_in 'search', with: 'Gothic'
+        click_on 'Search'
+      end
+      expect(current_path).to eq(shows_path)
+      expect(page).to have_content('Gothic', count: 1)
+      expect(page).to_not have_content('hi dive')
+      expect(page).to_not have_content('Bluebird')
+    end
+  end
 end
