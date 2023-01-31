d86074dfa3ab88202fde135aa50af5a6b4897059
Darren Kulback <dkulback@gmail.com>
2023-01-30 17:08:13 -0700

    Add string interpolation

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,5 +1,9 @@
 class Show < ApplicationRecord
   scope :by_date, lambda {
-                    where(date: Date.today..Date.today.next_year).order(:date)
-                  }
+    where(date: Date.today..Date.today.next_year).order(:date)
+  }
+
+  def self.search(search_params)
+    by_date.where('venue ILIKE ?', "%#{search_params}%")
+  end
 end
