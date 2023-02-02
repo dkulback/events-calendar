b3fc06397398e3115ec7b428d51904e1d78929ae
Darren Kulback <dkulback@gmail.com>
2023-02-01 17:45:11 -0700

    Fix date error model spec

--- spec/models/show_spec.rb
+++ spec/models/show_spec.rb
@@ -3,10 +3,11 @@ require 'rails_helper'
 RSpec.describe Show, type: :model do
   describe '::by_date' do
     it 'lets the shows by the date header' do
-      show0 = Show.create!(date: Date.yesterday)
+      show0 = Show.create!(date: Date.today.yesterday)
       show1 = Show.create!(date: Date.today)
       show2 = Show.create!(date: Date.tomorrow)
-      show3 = Show.create!(date: '31 Jan'.to_date)
+      show3 = Show.create!(date: Date.today.next_year)
+
       actual = Show.by_date
       expected = [show1, show2, show3]
       expect(actual).to eq(expected)
@@ -14,10 +15,10 @@ RSpec.describe Show, type: :model do
   end
   describe '::search' do
     it 'lets the shows by the date header and venue search param' do
-      show0 = Show.create!(date: Date.yesterday, venue: 'Gothic')
+      show0 = Show.create!(date: Date.today.yesterday, venue: 'Gothic')
       show1 = Show.create!(date: Date.today, venue: 'Bluebird')
       show2 = Show.create!(date: Date.tomorrow, venue: 'hi dive')
-      show3 = Show.create!(date: '31 Jan'.to_date.freeze, venue: 'Gothic')
+      show3 = Show.create!(date: Date.today.next_year.freeze, venue: 'Gothic')
       actual = Show.search('gothic')
       expected = [show3]
       expect(actual).to eq(expected)
