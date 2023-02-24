b86a4f12d8b2a790345818ff969bb964305ea668
Darren Kulback <dkulback@gmail.com>
2023-02-23 23:07:14 -0700

    Add access tokens

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_16_200427) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_23_022439) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -35,6 +35,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_16_200427) do
     t.datetime "remember_created_at"
     t.string "provider"
     t.string "uid"
+    t.string "access_token"
+    t.string "refresh_token"
+    t.bigint "expires_at"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
