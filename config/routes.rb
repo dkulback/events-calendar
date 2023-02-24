7348ac5f7ca862bd505e85a5433969dafc5bf62e
Darren Kulback <dkulback@gmail.com>
2023-02-23 23:05:59 -0700

    Add route for calendar

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   devise_for :users,
              controllers: { registrations: 'users/registraitions', sessions: 'users/sessions',
                             omniauth_callbacks: 'users/omniauth_callbacks' }
-  resources :shows
+  resources :shows do
+    post 'add_to_calendar', on: :member
+  end
 
   root to: 'shows#index'
 end
