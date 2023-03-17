bc967b8b62e876b9ca364f35584ac08a982bd982
Darren Kulback <dkulback@gmail.com>
2023-03-17 13:23:07 -0600

    Fix route typo

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users,
-             controllers: { registrations: 'users/registraitions', sessions: 'users/sessions',
+             controllers: { registrations: 'users/registrations', sessions: 'users/sessions',
                             omniauth_callbacks: 'users/omniauth_callbacks' }
   resources :shows do
     post 'add_to_calendar', on: :member
