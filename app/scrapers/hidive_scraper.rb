570af71a96175e5ce45051541639bbaca975228f
Darren Kulback <dkulback@gmail.com>
2023-07-01 10:53:13 -0600

    Update scraper for new html template

--- app/scrapers/hidive_scraper.rb
+++ app/scrapers/hidive_scraper.rb
@@ -3,42 +3,44 @@ class HidiveScraper
   def self.shows
     events = Connection.get_url(URL)
     shows = []
-    events.xpath("//div[@class='search-event-card-wrapper']").each do |event|
-      venue = event.xpath("//*[@id='root']/div/div[2]/div/div/div/div[1]/div/main/div/div/section[1]/div/section/div/div/section[2]/ul/li[3]/div/section/div/section[1]/div/p[2]").text
-      next unless venue == 'Hi-Dive • Denver, CO'
+    events.xpath("//div[@class='Container_root__16e3w NestedActionContainer_root__1jtfr event-card event-card__horizontal horizontal-event-card__action-visibility']").each do |event|
+      venue = event.css('.Typography_root__4bejd.Typography_body-md__4bejd.event-card__clamp-line--one.eds-text-color--ui-600.Typography_align-match-parent__4bejd')[1].text
 
+      next unless venue.include?('HI') || venue.include?('Hi')
+
+      date = event.css('.Typography_root__4bejd.Typography_body-md__4bejd.event-card__clamp-line--one.eds-text-color--ui-600.Typography_align-match-parent__4bejd')[0].text.squish.gsub(
+        '•', ''
+      )
       show = {}
-      date = event.css('.eds-event-card-content__primary-content').css('div').last.text
       if date.include?('Today')
         date = Date.today
       elsif date.include?('Tomorrow')
         date = Date.today.tomorrow
       end
-      show[:band] = event.css("[class='eds-is-hidden-accessible']").css('div').text
+      show[:band] = event.css('h2').text
       show[:date] = date.to_date unless date.blank?
       show[:venue] = venue
-      show[:tickets] = event.css('a')[0]['href']
       shows << show
-      require 'pry'
-      binding.pry
     end
 
     events = Connection.get_url('https://www.eventbrite.com/d/co--denver/hi-dive/?page=2')
-    events.xpath("//div[@class='eds-event-card-content__content__principal']").each do |event|
-      venue = event.css("[class='card-text--truncated__one']").text.squish
-      next unless venue == 'Hi-Dive • Denver, CO'
+    events.xpath("//div[@class='Container_root__16e3w NestedActionContainer_root__1jtfr event-card event-card__horizontal horizontal-event-card__action-visibility']").each do |event|
+      venue = event.css('.Typography_root__4bejd.Typography_body-md__4bejd.event-card__clamp-line--one.eds-text-color--ui-600.Typography_align-match-parent__4bejd')[1].text
 
+      next unless venue.include?('HI') || venue.include?('Hi')
+
+      date = event.css('.Typography_root__4bejd.Typography_body-md__4bejd.event-card__clamp-line--one.eds-text-color--ui-600.Typography_align-match-parent__4bejd')[0].text.squish.gsub(
+        '•', ''
+      )
       show = {}
-      date = event.css('.eds-event-card-content__primary-content').css('div').last.text
       if date.include?('Today')
         date = Date.today
       elsif date.include?('Tomorrow')
         date = Date.today.tomorrow
       end
-      show[:band] = event.css("[class='eds-is-hidden-accessible']").css('div').text
+      show[:band] = event.css('h2').text
       show[:date] = date.to_date unless date.blank?
       show[:venue] = venue
-      show[:tickets] = event.css('a')[0]['href']
       shows << show
     end
     shows
