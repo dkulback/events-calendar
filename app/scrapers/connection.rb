61d0e7dc3551b0e78aab97da2abcde816e72490d
Darren Kulback <dkulback@gmail.com>
2025-05-24 09:04:21 -0600

    Add open3 for web scraping

--- app/scrapers/connection.rb
+++ app/scrapers/connection.rb
@@ -1,7 +1,20 @@
 class Connection
+  OUTPUT = 'output.html'
   def self.get_url(url)
     conn = Faraday.new(url: url)
     response = conn.get
     Nokogiri::HTML(response.body)
+  end
+
+  def self.c_req(url)
+    _stdout, stderr, status = Open3.capture3("curl -o #{OUTPUT} #{url}")
+    if status.success?
+      file = File.open(OUTPUT)
+      doc = Nokogiri::HTML(file)
+      file.close
+      doc
+    else
+      puts "Error fetching HTML: #{stderr}"
+    end
   end
 end
